0ad275e1c30bf2e600181c57127eb6556442a281
groprog <ali1995ez@gmail.com>
2020-08-02 10:22:03 +0430

    add delete action

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { Card, Input,Button,Divider,List } from "antd";
-import { CheckSquareFilled } from '@ant-design/icons';
+import { CheckSquareFilled,DeleteOutlined } from '@ant-design/icons';
 export default function CardColumn(props) {
   const [task, setTask] = useState("");
   const [text, addText] = useState([]);
@@ -8,17 +8,33 @@ export default function CardColumn(props) {
     addText([...text,task]);
     setTask("")
   }
+  const deleteTask = (event,item) =>{
+     event.preventDefault()
+      let items = text.filter((t)=>{
+        return t != item
+      })
+    addText(items)
+  }
     const addButton = (
         <Button  type="primary" size="default" icon={<CheckSquareFilled/>} onClick={addTask}></Button>
     )
 
+
+  const onEnterPress = (e) =>{
+    if(e.keyCode == 13){
+      addTask();
+    }
+  }
+
   return (
     <Card title={props.title} bordered={false}>
-      <Input id="task" value={task} onChange={event => setTask(event.target.value) } placeholder="enter task" suffix={addButton} />
+      <Input id="task" value={task} onChange={event => setTask(event.target.value) } onKeyDown={(e)=>onEnterPress(e)} placeholder="enter task" suffix={addButton} />
       <Divider orientation="left">Tasks List</Divider>
         <List>
             {text.length? text.map(item=>(
-                <List.Item key={item}>{item}</List.Item>
+                <List.Item  extra={
+                  <Button type="default" size="default" icon={<DeleteOutlined/>} onClick={(e)=>deleteTask(e,item)}></Button>
+                } key={item}>{item}</List.Item>
             )):""}
         </List>
     </Card>
